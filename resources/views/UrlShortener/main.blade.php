7861d73b4a9791840df677ef85e50d6886f9ac24
Ivan <konovalov@sibwaypro.ru>
2017-10-19 02:12:32 +0700

    changed vanilla js to react

--- resources/views/UrlShortener/main.blade.php
+++ resources/views/UrlShortener/main.blade.php
@@ -1,5 +1,4 @@
 <!--resources/views/stcaddElement.blade.php-->
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,7 +7,11 @@
 	<title>my URL-shortener</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}" />
 	<script src="{{ asset('js/jquery.min.js') }}" defer></script> 
-	<script src="{{ asset('js/minUrl/url.js') }}" defer></script> 
+	<script src="{{ asset('js/minUrl/url.js') }}" defer></script>
+	<script src="{{ asset('js/react/react.min.js') }}" defer></script>
+	<script src="{{ asset('js/react/react-dom.min.js') }}" defer></script> 
+	<script src="{{ asset('js/react/browser.min.js') }}" defer></script>
+	<script src="{{ asset('js/react/axios.min.js') }}" defer></script> 	
 	<style type="text/css">
 		tr,th,td { border: 1px solid #BBBBBB; text-align: center;}
 		h1 {color:#007324;}
@@ -17,46 +20,46 @@
 		#Shorten {
 			background-color:#67DB8C;
 			border: 2px solid #02BA3C;
-			font: bold 18px verdana;} 
+			font: bold 18px verdana;
+		} 
 		#Shorten:hover {
 			background-color:#6BBAFF;
 		}
-		#ResMsg {
-			font-size:18px;
+		.message, .msg{
+			font: bold 16px verdana; 
+			color: #ffec04;
+			padding-left: 10px;
 		}
-		.msg {
-			font: bold 16px verdana; color: #ffec04;
+		.UrlInput {
+			margin-bottom: 20px; font: bold 16px verdana; 
+			width:80%;
+		}
+		.UrlOutput {
+			margin-top: 20px; 
+			font: bold 16px verdana; 
+			background-color: #82FF0F;
+			min-width: 300px;
 		}
 	</style>
 </head>
 <body>
 	<div class="col-sm-2"></div>
 	<div class="col-sm-8">
-	<h1 style="margin-bottom:40px;">my URL-shortener</h1>
-	<div class="row" style="height:40px;">
-    @if (session('message'))
-	<p class="msg">{{ session('message') }}</p>
-	@endif
-    </div>
-	<div class="row">
-		<div class="col-sm-10"><b>Введите оригинальный URL с указанием протокола (например&nbsp;http://&nbsp;или&nbsp;https://)</b></div> 
-		<div class="col-sm-2"></div> 
-	</div><br/>
-	<div class="row">
-		<div class="col-sm-10">{{Form::text('LongUrl','',array('id'=>'lUrl','style'=>'width:100%;'))}}</div> 
-		<div class="col-sm-2"></div>
+		<h1 style="margin-bottom:40px;">my URL-shortener</h1>	
+		<div class="row" style="height:40px;">
+	    @if (session('message'))
+		<p class="msg" style="padding-left:0;">{{ session('message') }}</p>
+		@endif
+	    </div>
+		<div class="row">
+			<div class="col-sm-10"><b>Введите оригинальный URL с указанием протокола (например&nbsp;http://&nbsp;или&nbsp;https://)</b></div> 
+			<div class="col-sm-2"></div> 
+		</div><br/>
+		<div id="UrlShortener">
+		</div>
 	</div>
-	<div class="row" style="margin-top:20px;">
-	<div class="col-sm-5">{{Form::text('','',array('id'=>'resUrl','disabled','style'=>'width:100%'))}}
-	</div>
-	<div id="ResMsg" class="col-sm-7">
-	</div>
-	</div>
-	<div style="margin-top:20px;">
-	<button id="Shorten">Сгенерировать</button>
-	</div>
-	</div>
-	<div class="col-sm-2"></div>
+	<div class="col-sm-2"></div>	
+	<script src = "{{ asset('js/minUrl/urlReact.js') }}" defer type="text/babel" ></script>
 </body>
 </html>
 
