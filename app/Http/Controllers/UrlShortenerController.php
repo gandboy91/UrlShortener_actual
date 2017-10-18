43bfb0c13229c843f944375e50775feca9ac9cf2
Ivan <konovalov@sibwaypro.ru>
2017-10-19 02:27:28 +0700

    tried to replace tab with 4spaces

--- app/Http/Controllers/UrlShortenerController.php
+++ app/Http/Controllers/UrlShortenerController.php
@@ -11,52 +11,52 @@ use Validator;
 
 class UrlShortenerController extends Controller
 {
-	public function main(Request $request)
-	{
-		if ($slug = $request->slug)
-			return $this->redirectBySlug($slug);
-	  	return view('UrlShortener.main');
-	}
+    public function main(Request $request)
+    {
+        if ($slug = $request->slug)
+            return $this->redirectBySlug($slug);
+        return view('UrlShortener.main');
+    }
 
-	public function addUrl(ShortenerUrlManager $UrlManager)
-	{
-	  	$responce = array();
-	  	$shortUrl = $hash = '';
-	  	$responceStatus = 200;
-		$validationStatus = $UrlManager->validateUrl();
-		if ($validationStatus === 200) {
-			$sanitizedUrl = $UrlManager->getSanitizedUrl();
-			$idOfUrl = $UrlManager->findIdByUrl($sanitizedUrl);
-			if ($idOfUrl!==0) {
-				$hash = UrlHasher::IdToHash($idOfUrl);
-			} else {
-				if ($newUrlId = $UrlManager->SaveUrlAndReturnId($sanitizedUrl))
-					$hash = UrlHasher::idToHash($newUrlId);
-				else 
-					$responceStatus = 500; // DB write error	
-			}
-			$shortUrl = url("$hash"); 
-		} else {
-			$responceStatus = $validationStatus;
-		}
-		$responce['status'] = $responceStatus;
-		$responce['shortUrl'] = $shortUrl;
-		return json_encode($responce);
-	}
+    public function addUrl(ShortenerUrlManager $UrlManager)
+    {
+        $responce = array();
+        $shortUrl = $hash = '';
+        $responceStatus = 200;
+        $validationStatus = $UrlManager->validateUrl();
+        if ($validationStatus === 200) {
+            $sanitizedUrl = $UrlManager->getSanitizedUrl();
+            $idOfUrl = $UrlManager->findIdByUrl($sanitizedUrl);
+            if ($idOfUrl!==0) {
+                $hash = UrlHasher::IdToHash($idOfUrl);
+            } else {
+                if ($newUrlId = $UrlManager->SaveUrlAndReturnId($sanitizedUrl))
+                    $hash = UrlHasher::idToHash($newUrlId);
+                else 
+                    $responceStatus = 500; // DB write error    
+            }
+            $shortUrl = url("$hash"); 
+        } else {
+            $responceStatus = $validationStatus;
+        }
+        $responce['status'] = $responceStatus;
+        $responce['shortUrl'] = $shortUrl;
+        return json_encode($responce);
+    }
 
-	private function redirectBySlug($slug)
-	{
-		$slug = trim($slug);
-		$message = '';
-		$statusCode = UrlChecker::checkSlug($slug);
-		if ($statusCode === 200) {
-			$idOfUrl = UrlHasher::hashToId($slug);
-			if ($longUrl = ShortenerUrlManager::findUrlById($idOfUrl))
-				return redirect($longUrl);
-			else
-				$statusCode = 410;
-		}
-		$message = UrlChecker::getErrorDescription($statusCode);
-		return redirect()->route('UrlShortener/main')->with('message',$message);
-	}
+    private function redirectBySlug($slug)
+    {
+        $slug = trim($slug);
+        $message = '';
+        $statusCode = UrlChecker::checkSlug($slug);
+        if ($statusCode === 200) {
+            $idOfUrl = UrlHasher::hashToId($slug);
+            if ($longUrl = ShortenerUrlManager::findUrlById($idOfUrl))
+                return redirect($longUrl);
+            else
+                $statusCode = 410;
+        }
+        $message = UrlChecker::getErrorDescription($statusCode);
+        return redirect()->route('UrlShortener/main')->with('message',$message);
+    }
 }
